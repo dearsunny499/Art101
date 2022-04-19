36c17affc3263584f71b08e78442fb67353382c4
dearsunny499 <yhu241@ucsc.edu>
2022-04-18 22:27:23 -0700

    Lab4

---
+++ lab4/js/lab.js
@@ -0,0 +1,14 @@
+// A cat game.
+// A game where user control a cat to move to a desinated location.
+// @link  https://dearsunny499.github.io/Art101/lab4/index.html
+// @file  lab4/js/lab.js
+// @author Yanyao Hu
+// @since  04/18/2022
+
+// Draw the background 
+// Draw the cat
+// Give the cat location to explore
+// On each frame
+//   Accept User input to control the cat
+//   Validate User's input
+//   Print win message if cat reach the location
---
+++ lab5/js/lab.js
@@ -0,0 +1,14 @@
+/*
+ * Author: Yanyao Hu <yhu241@ucsc.edu>
+ * Created:18 April
+ * License:
+ */
+
+ //  Define Variables
+make="";
+model="";
+color="";
+year=;
+
+// calculate
+age = 
