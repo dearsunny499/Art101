863e7bb75745a8065a82da92daa0a76db007750d
dearsunny499 <86376909+dearsunny499@users.noreply.github.com>
2022-04-27 00:30:53 -0700

    Update lab7.js

--- lab7/js/lab7.js
+++ lab7/js/lab7.js
@@ -8,7 +8,7 @@ function sortUserName() {
   var nameArray = userName.split('');
   console.log("nameArray =", nameArray);
   // sort the array
-  var nameArraySort = nameArray.sort();
+  var nameArraySort = nameArray.sort('');
   console.log("nameArraySort =", nameArraySort);
   //join array back to a string
   var nameSorted = nameArraySort.join('');
