db23e0fafa35a7fb07e285d5f0643abbcf1ab2f6
dearsunny499 <yhu241@ucsc.edu>
2022-05-03 06:28:14 -0700

    update Lab8

--- lab8/js/lab8.js
+++ lab8/js/lab8.js
@@ -4,7 +4,7 @@
  * License: Public Domain
 */
 
-function is Even(x){
+function isEven(x){
     return (x % 2 == 0);
 }
 
@@ -13,7 +13,7 @@ console.log("Is 1 even? ", isEven(1));
 console.log("Is 2 even?", isEven(2));
 
 array = [100, 81, 4, 16, 42, 144, 10000]
-console/log("My array", array);
+console.log("My array", array);
 
 var result = array.map(isEven);
 // should return (true, false, true, true, true, true, true)
@@ -23,4 +23,4 @@ var result = array.map(function(x){
     return x ** 0.5;
 })
 // should return {10, 9, 2, 4, 6.480740786, 12,100}
-console.log("Squaroot of array:", result)
+console.log("Squaroot of array:", result);
