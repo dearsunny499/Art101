1d7e994bcccbee37314548745ed9bea8f900c091
dearsunny499 <yhu241@ucsc.edu>
2022-05-09 22:03:44 -0700

    Lab10

---
+++ lab10/js/lab.js
@@ -0,0 +1,35 @@
+/*
+ * Author: Yanyao Hu<yhu241@ucsc/edu>
+ * Created: 9 May
+ * License: Public Domain
+*/
+
+var nameField = document.getElementById('name');
+var submitButton = document.getElementById('submit-button');
+var outputText = document.getElementById("output");
+
+function sortUserName() {
+  var userName = window.prompt("Hi. Please tell me your name so I can fix it.");
+  console.log("useName =", userName);
+  // split string to array
+  var nameArray = userName.split('');
+  console.log("nameArray =", nameArray);
+  // sort the array
+  var nameArraySort = nameArray.sort();
+  console.log("nameArraySort =", nameArraySort);
+  //join array back to a string
+  var nameSorted = nameArraySort.join('');
+  console.log("nameSorted =", nameSorted);
+  //Note that I could have done the above lines as a single line:
+  //  userName.tolower().split("").sort()join("")
+  return nameSorted;
+}
+
+
+submitButton.addEventLIstener('click', function(){
+  var name = nameField.value;
+  new1El = document.createElement("p");
+  new1El.innerHTML = sortUserName(name);
+
+  outputText.appendChild(new1El);
+});
